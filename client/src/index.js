0ab24c8ccb6aa829883225ced78f139ca3cdf253
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-09-19 19:44:17 -0300

    Create basic structure of the game

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,97 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Image from "react-bootstrap/Image";
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const gameImage = require('./images/game.jpg');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const pages = {
+            'PLAY': 'Play',
+            'RULES': 'Rules'
+        };
+
+        this.state = {
+            activePage: pages.PLAY,
+            pages: pages
+        };
+    }
+
+    setActivePage = (page) => {
+        this.setState({activePage: page});
+    };
+
+    renderActivePage = () => {
+        switch (this.state.activePage) {
+            case this.state.pages.PLAY:
+                return <Game/>
+            case this.state.pages.RULES:
+                return <Rules/>;
+            default:
+                return null
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header pages={this.state.pages}
+                        setActivePage={this.setActivePage}/>
+                {this.renderActivePage()}
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="body">
+                Not implemented
+            </div>
+        )
+    }
+}
+
+function Rules(props) {
+    return (
+        <div id="rules-body">
+            <Image src={gameImage} style={{height: 400}}/>
+            <p>
+                Gomoku, also called Five in a Row, is an abstract strategy board
+                game. It is traditionally played with Go pieces (black and white
+                stones) on a Go board.
+
+                Players alternate turns placing a stone of their color on an
+                empty intersection. The winner is the first player to form an
+                unbroken chain of five stones horizontally, vertically, or
+                diagonally.
+            </p>
+        </div>
+    )
+}
+
+function Header(props) {
+    return (
+        <Navbar bg="light" variant="light">
+            <Navbar.Brand href="#home">Five in a Row</Navbar.Brand>
+            <Nav className="mr-auto" defaultActiveKey={props.pages.PLAY}
+                 onSelect={(selectedKey) => props.setActivePage(selectedKey)}>
+                {Object.keys(props.pages).map(pageName => {
+                    const page = props.pages[pageName];
+                    return <Nav.Link key={page}
+                                     eventKey={page}>{page}</Nav.Link>
+                })}
+            </Nav>
+        </Navbar>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
