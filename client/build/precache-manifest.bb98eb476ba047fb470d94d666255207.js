6617d18b97c020ca38481eff6579365eb6828226
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-09-22 01:59:46 -0300

    Add build files

---
+++ client/build/precache-manifest.bb98eb476ba047fb470d94d666255207.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ddf35309d60b934fff8c4d11c4468c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c273cc7c09112fd35b73",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a4864b53a57f38dcd40d",
+    "url": "/static/css/main.b82a210e.chunk.css"
+  },
+  {
+    "revision": "c273cc7c09112fd35b73",
+    "url": "/static/js/2.2e537deb.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.2e537deb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4864b53a57f38dcd40d",
+    "url": "/static/js/main.951b1f62.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ecfa66efdce221c00e591b578d760710",
+    "url": "/static/media/game.ecfa66ef.jpg"
+  }
+]);
